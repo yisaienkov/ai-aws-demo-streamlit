2d1eae4d09f06450cda0911c0c5ba4f95d0b1b33
PΣTR PΣTRUK <petr.square@gmail.com>
2022-11-24 18:27:24 +0200

    Add save to s3 bucket. Update requirements.

--- src/dashboard.py
+++ src/dashboard.py
@@ -1,10 +1,12 @@
 import os
 import sys
+import time
 import base64
 import requests
 from io import BytesIO
 
 import cv2
+import boto3
 import numpy as np
 from PIL import Image
 import streamlit as st
@@ -50,7 +52,7 @@ def vis_image(image: PngImageFile, col: DeltaGenerator, text: str) -> None:
         st.image(image, width=350)
 
 
-def get_predict_image(ip: str, port: str) -> PngImageFile:
+def get_predict_by_image_file(ip: str, port: str) -> PngImageFile:
     response = requests.post(f"http://{ip}:{port}/api/v1/predict", files={"file": uploaded_file})
     return Image.open(BytesIO(response.content))
 
@@ -64,8 +66,21 @@ def get_image_download_link(img: PngImageFile, filename: str) -> str:
 
 
 if __name__ == "__main__":
+    INPUT_FOLDER = "Inputs"
+
+    BUCKET = os.getenv("BUCKET")
+
+    ACCESS_KEY = os.getenv("ACCESS_KEY")
+    SECRET_KEY = os.getenv("SECRET_KEY")
+
     ip = os.getenv("IP")
     port = os.getenv("PORT")
+
+    client = boto3.client(
+        's3',
+        aws_access_key_id=ACCESS_KEY,
+        aws_secret_access_key=SECRET_KEY,
+    )
 
     uploaded_file = st.file_uploader("", type="jpg")
     before_col, after_col = st.columns(2)
@@ -74,10 +89,18 @@ if __name__ == "__main__":
         logger.info("Load image")
 
         image = bytes_to_image(uploaded_file, logger)
+
+        with BytesIO() as out_img:
+            image.save(out_img, format='PNG')
+            out_img.seek(0)  # Without this line it fails
+        file_name = hash(time.time())
+        client.put_object(Body=out_img, Bucket=BUCKET,
+                          Key=f'{INPUT_FOLDER}/{file_name}.jpg')
+
         vis_image(image, before_col, "Before")
 
         try:
-            predict_image = get_predict_image(ip, port)
+            predict_image = get_predict_by_image_file(ip, port)
             vis_image(predict_image, after_col, "After")
         except Exception as e:
             logger.error(e)
